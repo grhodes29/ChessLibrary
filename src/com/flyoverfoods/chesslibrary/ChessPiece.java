9e978839e009a5e5d0787d84f4373bc872ae3b68
Giancarlo Rhodes <giancarlo.rhodes@gmail.com>
2018-10-03 09:32:01 -0500

    commit

--- src/com/flyoverfoods/chesslibrary/ChessPiece.java
+++ src/com/flyoverfoods/chesslibrary/ChessPiece.java
@@ -1,17 +1,29 @@
 package com.flyoverfoods.chesslibrary;
 
+
+/**
+ * @author      Giancarlo Rhodes <giancarlo.rhodes@gmail.com>
+ * @version     x.x                 (current version number of program)
+ * @since       x.x          (the version of the package this class was first added to)
+ */
 public class ChessPiece implements IChessPiece {
 
     private boolean _alive;
 
+
+
+
     @Override
     public boolean IsAlive() {
+
+
+
         return _alive;
     }
-
 
     public ChessPiece(){
         _alive = true;
     }
 
 }
+
